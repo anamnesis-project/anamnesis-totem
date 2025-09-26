3c989f0b8c6cce64f40b61714e9461a8de8c9764
MuriloUnten <murilo.unten@proton.me>
2025-09-26 15:32:47 -0300

    add test for temperature sensor and oximeter (heart rate works but spo2 doesnt)

---
+++ firmware/gy906-test/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial);
+
+    if (!mlx.begin()) {
+        Serial.println("Error connecting to MLX sensor. Check wiring.");
+        while (1);
+    };
+}
+
+void loop() {
+    Serial.print("Ambient temperature = "); 
+    Serial.print(mlx.readAmbientTempC());
+    Serial.print("°C");      
+    Serial.print("   ");
+    Serial.print("Object temperature = "); 
+    Serial.print(mlx.readObjectTempC()); 
+    Serial.println("°C");
+
+    Serial.println("-----------------------------------------------------------------");
+    delay(1000);
+}
