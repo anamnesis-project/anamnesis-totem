285d81432b62bd1c6477d1c9d9b59ec9a437950e
vdamasceno04 <vdamasceno04@gmail.com>
2025-11-12 23:02:40 -0300

    increase fw timeout

--- firmware_service.py
+++ firmware_service.py
@@ -13,7 +13,7 @@ FW_INPUT = "fw/input"
 FW_OUTPUT = "fw/output"
 
 try:
-    ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=30)
+    ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=40)
     ser.flush()
     print("Serial port connected")
 except serial.SerialException as e:
